7bc42972e18fd3e9b379b72183ee830a263b78d4
tiborchers <tiborchers@uc.cl>
2017-09-08 17:40:15 -0300

    change name of app; update gitignore to ignore lock file; add database name hardcoded

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,24 @@
+console.log('db environment name', process.env.DB_USERNAME);
+console.log('db pswd environment name', process.env.DB_PASSWORD);
 const config = {
   default: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || 'borcho',
+    password: process.env.DB_PASSWORD || "kame",
     dialect: process.env.DB_DIALECT || 'postgres',
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || "borcho",
     host: process.env.DB_HOST || '127.0.0.1',
   },
   development: {
     extend: 'default',
-    database: 'iic2513template_dev',
+    database: process.env.DB_NAME || "borcho", //'iic2513template_dev',
   },
   test: {
     extend: 'default',
-    database: 'iic2513template_test',
+    database: process.env.DB_NAME || "borcho", //'iic2513template_test',
   },
   production: {
     extend: 'default',
-    database: 'iic2513template_production',
+    database: process.env.DB_NAME || "borcho", //'iic2513template_production',
   },
 };
 
