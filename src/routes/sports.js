6a7b4190ef0853971b48e5639cde9d25935f5b52
PABLO PINO <pdpino@uc.cl>
2017-09-11 21:41:08 -0300

    Add view for a specific sport, and link in sports index

--- src/routes/sports.js
+++ src/routes/sports.js
@@ -4,7 +4,19 @@ const router = new KoaRouter();
 
 router.get('sports', '/', async (ctx) => {
   const sports = await ctx.orm.sport.findAll();
-  await ctx.render('sports/index', { sports });
+  await ctx.render('sports/index', {
+    sports,
+    sportPath: sport => ctx.router.url('sport', { id: sport.id }),
+  });
+});
+
+router.get('sport', '/:id', async (ctx) => {
+  const sport = await ctx.orm.sport.findById(ctx.params.id);
+  await ctx.render('sports/show', {
+    sport,
+    // initiatives,
+    // ongInitiativesPath: ctx.router.url('ongInitiatives', ong.id),
+  });
 });
 
 module.exports = router;
