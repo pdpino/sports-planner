fbeb1dcaecee8b12519a3469d1ab163092db7d47
PABLO PINO <pdpino@uc.cl>
2017-09-11 21:06:12 -0300

    add exportenv file to export database variables before running app

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,22 @@
-console.log('db environment name', process.env.DB_USERNAME);
-console.log('db pswd environment name', process.env.DB_PASSWORD);
 const config = {
   default: {
-    username: process.env.DB_USERNAME || 'borcho',
-    password: process.env.DB_PASSWORD || "kame",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     dialect: process.env.DB_DIALECT || 'postgres',
-    database: process.env.DB_NAME || "borcho",
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST || '127.0.0.1',
   },
   development: {
     extend: 'default',
-    database: process.env.DB_NAME || "borcho", //'iic2513template_dev',
+    database: process.env.DB_NAME, //'iic2513template_dev', // TODO: create different dbs for development, test, production
   },
   test: {
     extend: 'default',
-    database: process.env.DB_NAME || "borcho", //'iic2513template_test',
+    database: process.env.DB_NAME, //'iic2513template_test',
   },
   production: {
     extend: 'default',
-    database: process.env.DB_NAME || "borcho", //'iic2513template_production',
+    database: process.env.DB_NAME, //'iic2513template_production',
   },
 };
 
