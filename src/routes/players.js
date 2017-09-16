51738f20832866ea9cf5c294642ea1a495b39f20
PABLO PINO <pdpino@uc.cl>
2017-09-15 21:01:48 -0300

    fix some left out things from merge isMember

--- src/routes/players.js
+++ src/routes/players.js
@@ -4,6 +4,20 @@ const playerTeamsRouter = require('./playerTeams');
 
 const router = new KoaRouter();
 
+/** Calculate the age of the player given his birthday**/
+function calculateAge(birthday){
+  const today = new Date();
+
+  const year = birthday.substring(0,4);
+  const month = birthday.substring(5,7);
+  const day = birthday.substring(8,10);
+
+  const dateBirthday = new Date(year, month-1, day);
+  const diff =  today - dateBirthday;
+  const age = Math.floor (diff/(1000*60*60*24*365.25));
+  return age;
+}
+
 router.get('players', '/', async (ctx) => {
   const players = await ctx.orm.player.findAll();
   await ctx.render('players/index', {
@@ -36,20 +50,27 @@ router.get('playerNew', '/new', async (ctx) => {
   });
 });
 
-
-
 router.get('playerShow', '/:id', async (ctx) => {
-  const player= await ctx.orm.player.findById(ctx.params.id);
+  const player = await ctx.orm.player.findById(ctx.params.id);
+  const playSports = await player.getSports();
   const memberofTeams = await player.getTeams();
+  const playerAge = calculateAge(player.age);
   await ctx.render('players/show', {
     player,
+    playSports,
+    playerAge,
     memberofTeams,
     editPlayerPath: ctx.router.url('playerEdit',player.id),
     cancelPlayerPath: ctx.router.url('players'),
     editPlayerTeamPath: (team) => ctx.router.url('playerTeamEdit', { playerId: player.id, id: team.id } ),
+    editPlayerSportPath: (sport) => ctx.router.url('playerSportEdit', {
+      playerId: player.id,
+      id: sport.id }
+    ),
     createPlayerTeamsPath: ctx.router.url('playerTeamNew', { playerId: player.id } ),
+    createPlayerSportsPath: ctx.router.url('playerSportNew', { playerId: player.id } ),
     playersPath: ctx.router.url('players'),
-   });
+  });
 });
 
 router.patch('playerUpdate', '/:id', async (ctx) => {
@@ -83,20 +104,6 @@ router.get('playerEdit', '/:id/edit', async (ctx) => {
     cancelPath: ctx.router.url('playerShow', player.id),
     editPlayerTeamsPath: ctx.router.url('playerTeams',player.id),
   });
-});
-
-router.get('playerShow', '/:id', async (ctx) => {
-  const player = await ctx.orm.player.findById(ctx.params.id);
-  const playSports = await player.getSports();
-  await ctx.render('players/show', {
-    player,
-    playSports,
-    editPlayerPath: ctx.router.url('playerEdit', player.id),
-    editPlayerSportPath: (sport) => ctx.router.url('playerSportEdit', { playerId: player.id, id: sport.id } ),
-    createPlayerSportsPath: ctx.router.url('playerSportNew', { playerId: player.id } ),
-    playersPath: ctx.router.url('players'),
-   });
-  // TODO: merge with other playerShow
 });
 
 router.delete('playerDelete', '/:id', async (ctx) => {
