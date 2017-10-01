6da270fc6a61f12679f40f3e17b6f40430bd8fdb
tiborchers <tiborchers@uc.cl>
2017-09-30 23:54:09 -0300

    Add model isInvited

---
+++ src/models/isinvited.js
@@ -0,0 +1,26 @@
+module.exports = function defineisInvited(sequelize, DataTypes) {
+  const isInvited = sequelize.define('isInvited', {
+    isAdmin: {
+      type:DataTypes.BOOLEAN,
+      defaultValue:false,
+    }
+    status: {
+      type: DataTypes.ENUM,
+      values: ['sentToUser', 'sentbyUser', 'userRejected','rejectdByUser',"accepted"] // HACK: copied in migration (and probably in routes/isInviteds)
+      allowNull: false,
+      validate: {
+        // NOTE: notNull has been deprecated, a warning is raised
+        // notNull: {
+        //   msg: "Estado no asignado"
+        // },
+        notEmpty: {
+          msg: "Estado no asignado"
+        },
+      },
+    },
+  });
+  isInvited.associate = function associate(models) {
+  };
+
+  return isInvited;
+};
