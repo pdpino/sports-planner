798aab49baeed1f22dd502b0f382b2660d3e629b
tiborchers <tiborchers@uc.cl>
2017-10-03 18:40:46 -0300

    Add field model and associations

---
+++ src/routes/fields.js
@@ -0,0 +1,86 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+
+
+router.get('fields', '/', async (ctx) => {
+  const fields = await ctx.orm.field.findAll();
+  await ctx.render('fields/index', {
+    fields,
+    fieldPath: field => ctx.router.url('field', { id: field.id }),
+    newFieldPath: ctx.router.url('fieldNew'),
+  });
+});
+
+router.get('fieldNew', '/new', async (ctx) => {
+  const field = ctx.orm.field.build();
+  await ctx.render('fields/new', {
+    field,
+    submitFieldPath: ctx.router.url('fieldCreate'),
+    cancelPath: ctx.router.url('fields'),
+  });
+});
+
+router.post('fieldCreate', '/', async (ctx) => {
+  try {
+    const field = await ctx.orm.field.create(ctx.request.body);
+
+    ctx.redirect(ctx.router.url('field', { id: field.id }));
+  } catch (validationError) {
+    await ctx.render('fields/new', {
+      field: ctx.orm.field.build(ctx.request.body),
+      errors: validationError.errors,
+      submitFieldPath: ctx.router.url('fieldCreate'),
+      cancelPath: ctx.router.url('fields'),
+    });
+  }
+});
+
+router.get('fieldEdit', '/:id/edit', async (ctx) => {
+  const field = await ctx.orm.field.findById(ctx.params.id);
+
+  await ctx.render('fields/edit', {
+    field,
+    submitFieldPath: ctx.router.url('fieldUpdate', field.id),
+    deleteFieldPath: ctx.router.url('fieldDelete', field.id),
+    cancelPath: ctx.router.url('field', { id: ctx.params.id }),
+  });
+});
+
+router.patch('fieldUpdate', '/:id', async (ctx) => {
+  const field = await ctx.orm.field.findById(ctx.params.id);
+  try {
+    await field.update(ctx.request.body);
+    ctx.redirect(ctx.router.url('field', { id: field.id }));
+  } catch (validationError) {
+    await ctx.render('fields/edit', {
+      field,
+      errors: validationError.errors,
+      submitFieldPath: ctx.router.url('fieldUpdate', field.id),
+      cancelPath: ctx.router.url('field', { id: ctx.params.id }),
+      deleteFieldPath: ctx.router.url('fieldDelete', field.id),
+    });
+  }
+});
+
+router.get('field', '/:id', async (ctx) => {
+  const field = await ctx.orm.field.findById(ctx.params.id);
+  await ctx.render('fields/show', {
+    field,
+    fieldOwner,
+    fieldsPath: ctx.router.url('fields'),
+    fieldOwnerPath: fieldOwner => ctx.router.url('fieldOwner', { id: fieldOwner.id }),
+    editFieldPath: ctx.router.url('fieldEdit', field.id),
+
+  });
+});
+
+router.delete('fieldDelete', '/:id', async (ctx) => {
+  const field = await ctx.orm.field.findById(ctx.params.id);
+  await field.destroy();
+  ctx.redirect(ctx.router.url('fields'));
+});
+
+
+module.exports = router;
