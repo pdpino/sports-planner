a04ecabe8e18a1021357d18643737d670bc36cb3
Pablo Pino <pdpino@uc.cl>
2017-10-03 16:31:52 -0300

    Add afterHook find for compoundOwner

--- src/models/compoundowner.js
+++ src/models/compoundowner.js
@@ -1,3 +1,14 @@
+// HACK: this is copied from models/players.js
+async function getUserObject(models, userId){
+  const user = await models.user.findById(userId);
+  return { // REVIEW: replace this by a js method (like assign)?
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    photo: user.photo,
+  };
+}
+
 module.exports = function definecompoundOwner(sequelize, DataTypes) {
   const compoundOwner = sequelize.define('compoundOwner', {
     phone: DataTypes.STRING,
@@ -6,6 +17,21 @@ module.exports = function definecompoundOwner(sequelize, DataTypes) {
     compoundOwner.belongsTo(models.user);
     compoundOwner.hasMany(models.compound);
   };
+
+  /** Load user info (email, names and photo) into player object
+   HACK: copied from models/player**/
+  compoundOwner.afterFind(async function loadUser(result) {
+    // REVIEW: avoid DB query?
+    if(result.constructor == Array) {
+      for (let i = 0; i < result.length; i++) {
+          Object.assign(result[i], await getUserObject(sequelize.models, result[i].userId));
+      }
+    } else {
+      Object.assign(result, await getUserObject(sequelize.models, result.userId));
+    }
+
+  });
+
 
   return compoundOwner;
 };
