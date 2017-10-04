4cefd18545bb93e30018ba555f2a58aaf0f709f5
Pablo Pino <pdpino@uc.cl>
2017-10-04 18:15:31 -0300

    Add failed hook to set a default name in match

--- src/models/match.js
+++ src/models/match.js
@@ -15,5 +15,17 @@ module.exports = function definematch(sequelize, DataTypes) {
     match.belongsToMany(models.player, { through: models.isPlayerInvited });
     match.belongsToMany(models.team, { through: models.isTeamInvited });
   };
+
+  // async function assertNotEmptyName(instance){
+  //   if (!instance.name){
+  //     const sport = await sequelize.models.sport.findById(instance.sportId);
+  //     return instance.set('name', 'Partido de ' + sport.name);
+  //   }
+  // }
+  //
+  // /** Set a default name **/
+  // match.afterCreate(assertNotEmptyName);
+  // match.afterUpdate(assertNotEmptyName); // REVIEW: players no pueden dejar match.name vacio?
+
   return match;
 };
