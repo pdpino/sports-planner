4226f7553076d8e2c2b3fb6442d67690401cc6ff
Pablo Pino <pdpino@uc.cl>
2017-10-22 12:39:46 -0300

    Add files example, not working

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -14,6 +14,12 @@ router.post('hello', '/', async (ctx) => {
   ctx.flashMessage.notice = 'Form successfully processed';
   // this is just to show how to send an e-mail using a mailer helper fn
   // but it will never be executed
+  // const uploads = ctx.request.body.files.uploads;
+  // if (Array.isArray(uploads)) {
+  //   uploads.forEach(f => fileStorage.upload(f));
+  // } else {
+  //   await fileStorage.upload(ctx.request.body.files.uploads);
+  // }
   if (Math.random() > 1) {
     await sendExampleEmail(ctx.request.body);
   }
